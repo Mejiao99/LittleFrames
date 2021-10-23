aaaa396f4e3368c8acafa143148b1b06f18cca4f
Mejiao <Jonathan19991@gmail.com>
2021-10-22 19:51:38 -0500

    noSubmit in methods

--- src/main/java/team/creative/littleframes/client/display/FrameVideoDisplay.java
+++ src/main/java/team/creative/littleframes/client/display/FrameVideoDisplay.java
@@ -1,15 +1,9 @@
 package team.creative.littleframes.client.display;
 
-import java.nio.ByteBuffer;
-import java.util.HashSet;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import org.lwjgl.opengl.GL11;
-
 import com.creativemd.creativecore.common.utils.mc.TickUtils;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
+import org.lwjgl.opengl.GL11;
 import team.creative.littleframes.client.texture.TextureCache;
 
 import uk.co.caprica.vlcj.factory.MediaPlayerFactory;
@@ -19,14 +13,19 @@ import uk.co.caprica.vlcj.player.embedded.videosurface.callback.BufferFormat;
 import uk.co.caprica.vlcj.player.embedded.videosurface.callback.BufferFormatCallback;
 import uk.co.caprica.vlcj.player.embedded.videosurface.callback.RenderCallback;
 
+import java.nio.ByteBuffer;
+import java.util.HashSet;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+
 public class FrameVideoDisplay extends FrameDisplay {
-    
-    private static HashSet<Runnable> toBeRun = new HashSet<>();
+
     private static final String VLC_DOWNLOAD_32 = "https://i.imgur.com/qDIb9iV.png";
     private static final String VLC_DOWNLOAD_64 = "https://i.imgur.com/3EKo7Jx.png";
     private static final int ACCEPTABLE_SYNC_TIME = 1000;
     private static boolean isVLCInstalled = true;
-    
+
+
     public static FrameDisplay createVideoDisplay(String url, float volume, boolean loop) {
         try {
             if (isVLCInstalled)
@@ -41,7 +40,7 @@ public class FrameVideoDisplay extends FrameDisplay {
             return cache.createDisplay(failURL, volume, loop, true);
         return null;
     }
-    
+
     public int width = 1;
     public int height = 1;
     public CallbackMediaPlayerComponent player;
@@ -51,13 +50,14 @@ public class FrameVideoDisplay extends FrameDisplay {
     private float lastSetVolume;
     private AtomicBoolean needsUpdate = new AtomicBoolean(false);
     private boolean first = true;
-    
+
     public FrameVideoDisplay(String url, float volume, boolean loop) {
         super();
+
+
         texture = GlStateManager.generateTexture();
-        
         player = new CallbackMediaPlayerComponent(new MediaPlayerFactory("--quiet"), null, null, false, new RenderCallback() {
-            
+
             @Override
             public void display(MediaPlayer mediaPlayer, ByteBuffer[] nativeBuffers, BufferFormat bufferFormat) {
                 synchronized (this) {
@@ -66,7 +66,7 @@ public class FrameVideoDisplay extends FrameDisplay {
                 }
             }
         }, new BufferFormatCallback() {
-            
+
             @Override
             public BufferFormat getBufferFormat(int sourceWidth, int sourceHeight) {
                 synchronized (this) {
@@ -74,25 +74,28 @@ public class FrameVideoDisplay extends FrameDisplay {
                     FrameVideoDisplay.this.height = sourceHeight;
                     FrameVideoDisplay.this.first = true;
                 }
-                return new BufferFormat("RGBA", sourceWidth, sourceHeight, new int[] { sourceWidth * 4 }, new int[] { sourceHeight });
+                return new BufferFormat("RGBA", sourceWidth, sourceHeight, new int[]{sourceWidth * 4}, new int[]{sourceHeight});
             }
-            
+
             @Override
             public void allocatedBuffers(ByteBuffer[] buffers) {
-            
+
             }
-            
+
         }, null);
-        player.mediaPlayer().submit(() -> {
-            player.mediaPlayer().audio().setVolume((int) (volume * 100F));
-            lastSetVolume = volume;
-            player.mediaPlayer().controls().setRepeat(loop);
-            player.mediaPlayer().media().start(url);
-        });
+
+        player.mediaPlayer().audio().setVolume((int) (volume * 100F));
+        lastSetVolume = volume;
+        player.mediaPlayer().controls().setRepeat(loop);
+        player.mediaPlayer().media().start(url);
+
     }
-    
+
     @Override
     public void prepare(String url, float volume, boolean playing, boolean loop, int tick) {
+        if (player == null) {
+            return;
+        }
         synchronized (this) {
             if (needsUpdate.getAndSet(false)) {
                 if (buffer != null && first) {
@@ -111,90 +114,85 @@ public class FrameVideoDisplay extends FrameDisplay {
         }
         if (player.mediaPlayer().media().isValid()) {
             boolean realPlaying = playing && !Minecraft.getMinecraft().isGamePaused();
-            
+
             if (volume != lastSetVolume) {
-                player.mediaPlayer().submit(() -> player.mediaPlayer().audio().setVolume((int) (volume * 100F)));
+                player.mediaPlayer().audio().setVolume((int) (volume * 100F));
                 lastSetVolume = volume;
             }
-            if (player.mediaPlayer().controls().getRepeat() != loop)
-                player.mediaPlayer().submit(() -> player.mediaPlayer().controls().setRepeat(loop));
+            if (player.mediaPlayer().controls().getRepeat() != loop) {
+                player.mediaPlayer().controls().setRepeat(loop);
+            }
             long tickTime = 50;
             long newDuration = player.mediaPlayer().status().length();
             if (!stream && newDuration != -1 && newDuration != 0 && player.mediaPlayer().media().info().duration() == 0)
                 stream = true;
             if (stream) {
-                if (player.mediaPlayer().status().isPlaying() != realPlaying)
-                    player.mediaPlayer().submit(() -> player.mediaPlayer().controls().setPause(!realPlaying));
+                if (player.mediaPlayer().status().isPlaying() != realPlaying) {
+                    player.mediaPlayer().controls().setPause(!realPlaying);
+                }
             } else {
                 if (player.mediaPlayer().status().length() > 0) {
                     long time = tick * tickTime + (realPlaying ? (long) (TickUtils.getPartialTickTime() * tickTime) : 0);
                     if (player.mediaPlayer().status().isSeekable() && time > player.mediaPlayer().status().time())
                         if (loop)
                             time %= player.mediaPlayer().status().length();
-                    if (Math.abs(time - player.mediaPlayer().status().time()) > ACCEPTABLE_SYNC_TIME)
-                        player.mediaPlayer().submit(() -> {
-                            long newTime = tick * tickTime + (realPlaying ? (long) (TickUtils.getPartialTickTime() * tickTime) : 0);
-                            if (player.mediaPlayer().status().isSeekable() && newTime > player.mediaPlayer().status().length())
-                                if (loop)
-                                    newTime %= player.mediaPlayer().status().length();
-                                
-                            player.mediaPlayer().controls().setTime(newTime);
-                            if (player.mediaPlayer().status().isPlaying() != realPlaying)
-                                player.mediaPlayer().controls().setPause(!realPlaying);
-                        });
+                    if (Math.abs(time - player.mediaPlayer().status().time()) > ACCEPTABLE_SYNC_TIME) {
+                        long newTime = tick * tickTime + (realPlaying ? (long) (TickUtils.getPartialTickTime() * tickTime) : 0);
+                        if (player.mediaPlayer().status().isSeekable() && newTime > player.mediaPlayer().status().length())
+                            if (loop)
+                                newTime %= player.mediaPlayer().status().length();
+
+                        player.mediaPlayer().controls().setTime(newTime);
+                        if (player.mediaPlayer().status().isPlaying() != realPlaying)
+                            player.mediaPlayer().controls().setPause(!realPlaying);
+
+                    }
                 }
             }
         }
     }
-    
+
     @Override
     public void release() {
-        Runnable run = new Runnable() {
-            
-            @Override
-            public void run() {
-                player.release();
-                synchronized (toBeRun) {
-                    toBeRun.remove(this);
-                }
-            }
-        };
-        
-        synchronized (toBeRun) {
-            toBeRun.add(run);
+        if (player == null) {
+            return;
         }
-        player.mediaPlayer().submit(run);
+        player.release();
+        player = null;
     }
-    
+
     @Override
     public int texture() {
         return texture;
     }
-    
+
     @Override
     public void pause(String url, float volume, boolean playing, boolean loop, int tick) {
-        player.mediaPlayer().submit(() -> {
-            player.mediaPlayer().controls().setTime(tick * 50);
-            player.mediaPlayer().controls().pause();
-        });
+        if (player == null) {
+            return;
+        }
+        player.mediaPlayer().controls().setTime(tick * 50);
+        player.mediaPlayer().controls().pause();
     }
-    
+
     @Override
     public void resume(String url, float volume, boolean playing, boolean loop, int tick) {
-        player.mediaPlayer().submit(() -> {
-            player.mediaPlayer().controls().setTime(tick * 50);
-            player.mediaPlayer().controls().play();
-        });
+        if (player == null) {
+            return;
+        }
+        player.mediaPlayer().controls().setTime(tick * 50);
+        player.mediaPlayer().controls().play();
+
     }
-    
+
     @Override
     public int getWidth() {
         return width;
     }
-    
+
     @Override
     public int getHeight() {
         return height;
     }
-    
+
 }
--- src/main/java/team/creative/littleframes/common/packet/CreativeFramePacket.java
+++ src/main/java/team/creative/littleframes/common/packet/CreativeFramePacket.java
@@ -1,7 +1,6 @@
 package team.creative.littleframes.common.packet;
 
 import com.creativemd.creativecore.common.packet.CreativeCorePacket;
-
 import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
@@ -9,38 +8,42 @@ import net.minecraft.util.math.BlockPos;
 import team.creative.littleframes.common.block.TileEntityCreativeFrame;
 
 public class CreativeFramePacket extends CreativeCorePacket {
-    
+
     public BlockPos pos;
     public boolean playing;
     public int tick;
-    
-    public CreativeFramePacket() {}
-    
+
+    public CreativeFramePacket() {
+    }
+
     public CreativeFramePacket(BlockPos pos, boolean playing, int tick) {
         this.pos = pos;
         this.playing = playing;
         this.tick = tick;
     }
-    
+
     @Override
     public void writeBytes(ByteBuf buf) {
         writePos(buf, pos);
         buf.writeBoolean(playing);
         buf.writeInt(tick);
     }
-    
+
     @Override
     public void readBytes(ByteBuf buf) {
         pos = readPos(buf);
         playing = buf.readBoolean();
         tick = buf.readInt();
     }
-    
+
     @Override
     public void executeClient(EntityPlayer player) {
-        TileEntity te = player.world.getTileEntity(pos);
-        if (te instanceof TileEntityCreativeFrame) {
-            TileEntityCreativeFrame frame = (TileEntityCreativeFrame) te;
+        TileEntity tileEntity = player.world.getTileEntity(pos);
+        if (tileEntity instanceof TileEntityCreativeFrame) {
+            TileEntityCreativeFrame frame = (TileEntityCreativeFrame) tileEntity;
+            if (frame.display == null) {
+                return;
+            }
             frame.playing = playing;
             frame.tick = tick;
             if (playing)
@@ -49,10 +52,10 @@ public class CreativeFramePacket extends CreativeCorePacket {
                 frame.display.pause(frame.getURL(), frame.volume, frame.playing, frame.loop, frame.tick);
         }
     }
-    
+
     @Override
     public void executeServer(EntityPlayer player) {
-        
+
     }
-    
+
 }
